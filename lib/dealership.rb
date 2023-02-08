2c626847d51b37e67975a8261099f4df15b53865
Thomas Hawley <thawley22@turing.edu>
2023-02-08 09:43:42 -0700

    feat: Add inventory and inventory count methods

---
+++ lib/dealership.rb
@@ -0,0 +1,12 @@
+class Dealership
+  attr_reader :inventory
+  def initialize(name, address)
+    @name = name
+    @address = address
+    @inventory = []
+  end
+
+  def inventory_count
+    @inventory.length
+  end
+end
