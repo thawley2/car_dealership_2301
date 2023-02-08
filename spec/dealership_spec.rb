5d1d833d4ed4ab6be1d00e2c41d7eecae46578af
Thomas Hawley <thawley22@turing.edu>
2023-02-08 09:41:23 -0700

    test: Add inventory test

--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -8,4 +8,16 @@ RSpec.describe Dealership do
 
     expect(dealership).to be_instance_of Dealership
   end
+
+  it 'has inventory' do
+    dealership = Dealership.new("Acme Auto", "123 Main Street")
+
+    expect(dealership.inventory).to eq([])
+  end
+
+  it 'can count inventory' do
+    dealership = Dealership.new("Acme Auto", "123 Main Street")
+
+    expect(dealership.inventory_count).to eq(0)
+  end
 end
