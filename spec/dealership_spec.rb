7a79dfda7a7d9f0d3b99c93b4097afacc6999fbf
Thomas Hawley <thawley22@turing.edu>
2023-02-08 09:56:22 -0700

    test: Add test for has_inventory?

--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -31,4 +31,25 @@ RSpec.describe Dealership do
     expect(dealership.inventory).to eq([car_1, car_2])
     expect(dealership.inventory_count).to eq(2)
   end
+
+  it 'has inventory checker for no inventory' do
+    dealership = Dealership.new("Acme Auto", "123 Main Street")
+
+    expect(dealership.has_inventory?).to be false
+  end
+
+  it 'has inventory checker for inventory' do
+    dealership = Dealership.new("Acme Auto", "123 Main Street")
+    car_1 = Car.new("Ford Mustang", 1500, 36)
+    car_2 = Car.new("Toyota Prius", 1000, 48)
+    car_3 = Car.new("Toyota Tercel", 500, 48)
+    car_4 = Car.new("Chevrolet Bronco", 1250, 24)
+    dealership.add_car(car_1)
+    dealership.add_car(car_2)
+    dealership.add_car(car_3)
+    dealership.add_car(car_4)
+
+    expect(dealership.has_inventory?).to be true
+
+  end
 end
